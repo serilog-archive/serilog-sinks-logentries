dfcf85c8ab459cec0dd251f6b4dc1b74056a4ed2
Shawn Anderson <sanderson@eye-catcher.com>
2015-07-11 20:37:11 -0400

    Converted to PCL Project Updated to leverage sockets-for-pcl library Added Unit Tests

---
+++ test/Serilog.Sinks.Logentries.Tests/LeClient.Test.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog.Sinks.Logentries;
+
+namespace Serilog.Sinks.Logentries.Tests
+{
+    [TestFixture]
+    public class LeClientTests
+    {
+        [SetUp]
+        public void Initialize()
+        {
+
+        }
+
+        [Test]
+        public void TestTcpTokenWriteHttp()
+        {
+            var client = new LeClient(false, false);
+
+            ExecuteTest(client);
+        }
+
+        [Test]
+        public void TestTcpTokenWriteHttps()
+        {
+            var client = new LeClient(false, true);
+
+            ExecuteTest(client);
+        }
+
+        [Test]
+        public void TestHttpPutWriteHttp()
+        {
+            var client = new LeClient(true, false, accountKey: "", hostKey: "General Tests", logKey: "");
+
+            ExecuteTest(client);
+        }
+
+        [Test]
+        public void TestHttpPutWriteHttps()
+        {
+            var client = new LeClient(true, true, accountKey: "", hostKey:"General Tests", logKey: "");
+
+            ExecuteTest(client);
+        }
+
+        void ExecuteTest(LeClient client)
+        {
+            client.Connect();
+            client.Write("605885c4-a4d4-42c7-a3f4-f2f97b124ca1" + " HTTPS test log entry\n");
+            client.Flush();
+            client.Close();
+        }
+    }
+}
---
+++ test/Serilog.Sinks.Logentries.Tests/LogentriesSink.Test.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Serilog.Tests.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.Logentries.Tests
+{
+    [TestFixture]
+    public class LogentriesSinkTests
+    {
+        [SetUp]
+        public void Initialize()
+        {
+
+        }
+
+        [Test]
+        public void TestLogWrite()
+        {
+            var log = new LoggerConfiguration()
+                .WriteTo.Logentries("", outputTemplate: "[{Level}] {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            log.Write(Some.InformationEvent());
+
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
